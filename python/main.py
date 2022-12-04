4442437149dc692c1a5376e74ae8f9665381f338
c-bruce <callum.bruce1@gmail.com>
2022-12-04 14:36:08 +0000

    Update with fixed Kalman gain

--- python/main.py
+++ python/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # GET DATA
-df = pd.read_csv('002_mpu6050_RAW.csv')
+df = pd.read_csv('003_mpu6050_RAW.csv')
 
 # CONSTANTS
 dt = 0.004 # [s]
@@ -44,11 +44,6 @@ def vectorized_integration(input_vector, dt):
 def get_angle_from_acc(a, b):
     return np.degrees(np.arctan2(b, a) + 1.5)
 
-# def get_alpha_from_acc(AccX, AccY, AccZ):
-#     total_vector_acc = np.sqrt(sum([AccX**2, AccY**2, AccZ**2]))
-#     print(total_vector_acc)
-#     return np.arcsin(AccY/total_vector_acc) * -57.296
-
 def get_angle_from_complimentary_filter(a, b, input_vector, dt):
     output_vector = []
     alpha_acc = get_angle_from_acc(a, b)
@@ -72,28 +67,13 @@ time = get_time(df, dt, freq)
 AccX_SU, AccY_SU, AccZ_SU, GyroX_SU, GyroY_SU, GyroZ_SU = get_raw_in_sensor_units(df)
 AccX_PU, AccY_PU, AccZ_PU, GyroX_PU, GyroY_PU, GyroZ_PU = get_raw_in_physical_units(df, acc_scale_factor, gyro_scale_factor)
 
-AngX_PU = vectorized_integration(GyroX_PU, dt)
-AngY_PU = vectorized_integration(GyroY_PU, dt)
-AngZ_PU = vectorized_integration(GyroZ_PU, dt)
+# AngX_PU = vectorized_integration(GyroX_PU, dt)
+# AngY_PU = vectorized_integration(GyroY_PU, dt)
+# AngZ_PU = vectorized_integration(GyroZ_PU, dt)
 
-# alpha = AngY_PU
 alpha = get_angle_from_acc(AccX_PU, AccZ_PU)
-# alpha = get_angle_from_complimentary_filter(AccX_PU, AccZ_PU, GyroY_PU, dt)
+# alpha[alpha > 90] = 0
 alpha_CF = get_angle_from_complimentary_filter(AccX_PU, AccZ_PU, GyroY_PU, dt)
-# alpha = get_alpha_from_acc(AccX_PU, AccY_PU, AccZ_PU)
-
-# plt.plot(time, AccX_PU)
-# plt.plot(time, AccY_PU)
-# plt.plot(time, AccZ_PU)
-
-# plt.plot(time, GyroX_PU)
-# plt.plot(time, GyroY_PU)
-# plt.plot(time, GyroZ_PU)
-
-# plt.plot(time, AngX_PU)
-# plt.plot(time, AngY_PU)
-# plt.plot(time, AngZ_PU)
-# plt.show()
 
 # KALMAN FILTER
 # Constant matricies
@@ -106,36 +86,9 @@ Q_k = np.array([[0, 0],
                 [0, 0]]) # Untracked noise
 H_k = np.array([[1, 0],
                 [0, 1]]) # Transformation matrix
-# Step 0: Get initial state and covariance matrix
-# Use first 0.04 seconds of raw alpha and GyroY_PU data
-# window = int(0.04 / dt) # Size of window for getting sensor readings over
-# x_k = np.array([np.mean(alpha[0:window]), np.mean(GyroY_PU[0:window])])
-# P_k = np.array([[variance(alpha[0:window]), covariance(alpha[0:window], GyroY_PU[0:window])],
-#                 [covariance(GyroY_PU[0:window], alpha[0:window]), variance(GyroY_PU[0:window])]])
-#
-# Step 1: Prediction step
-# Get prediction
-# x_k = np.dot(F_k, x_k) + np.dot(B_k, u_k)
-# P_k = np.dot(F_k, np.dot(P_k, F_k.T)) + Q_k
-# # Print prediction
-# print(f"Predicted x_k: {x_k}")
-# print(f"Predicted P_k: {P_k}")
+I = np.array([[1, 0],
+              [0, 1]]) # Identity matrix
 
-# # Step 2: Update step
-# # Get new sensor readings
-# z_k = np.array([np.mean(alpha[1:window+1]), np.mean(GyroY_PU[1:window+1])])
-# R_k = np.array([[variance(alpha[1:window+1]), covariance(alpha[1:window+1], GyroY_PU[1:window+1])],
-#                 [covariance(GyroY_PU[1:window+1], alpha[1:window+1]), variance(GyroY_PU[1:window+1])]])
-# # Calculate Kalman gain
-# K = np.dot(P_k, np.dot(H_k.T, np.linalg.inv(np.dot(H_k, np.dot(P_k, H_k)) + R_k)))
-# # Calculate updated x_k and P_k
-# x_k = x_k + np.dot(K, (z_k - np.dot(H_k, x_k)))
-# P_k = P_k - np.dot(K, np.dot(H_k, P_k))
-
-# print(f"Updated x_k: {x_k}")
-# print(f"Updated P_k: {P_k}")
-
-# Put it all in a loop...
 def get_prediction(F_k, x_k, B_k, u_k, P_k, Q_k):
     x_k = np.dot(F_k, x_k) + np.dot(B_k, u_k)
     P_k = np.dot(F_k, np.dot(P_k, F_k.T)) + Q_k
@@ -143,53 +96,60 @@ def get_prediction(F_k, x_k, B_k, u_k, P_k, Q_k):
 
 def get_new_sensor_readings(start, end):
     z_k = np.array([np.mean(alpha[start:end]), np.mean(GyroY_PU[start:end])])
+    #z_k = np.array([alpha[end], GyroY_PU[end]])
     R_k = np.array([[variance(alpha[start:end]), covariance(alpha[start:end], GyroY_PU[start:end])],
                     [covariance(GyroY_PU[start:end], alpha[start:end]), variance(GyroY_PU[start:end])]])
     return z_k, R_k
 
 def get_kalman_gain(P_k, H_k, R_k):
-    return np.dot(P_k, np.dot(H_k.T, np.linalg.inv(np.dot(H_k, np.dot(P_k, H_k.T)) + R_k)))
+    K = np.dot(P_k, np.dot(H_k.T, np.linalg.inv(np.dot(H_k, np.dot(P_k, H_k.T)) + R_k)))
+    return K
 
-def get_update(x_k, K, z_k, H_k, P_k):
-    x_k = x_k + np.dot(K, (z_k - np.dot(H_k, x_k)))
-    P_k = P_k - np.dot(K, np.dot(H_k, P_k))
-    return x_k, P_k
+def get_state_update(x_k, K, z_k, H_k, P_k):
+    return x_k + np.dot(K, (z_k - np.dot(H_k, x_k)))
+
+def get_covariance_update(I, K, H_k, P_k, R_k):
+    return np.dot((I - np.dot(K, H_k)), np.dot(P_k, (I - np.dot(K, H_k)).T)) + np.dot(K, np.dot(R_k, K.T))
 
 # Step 0: Get initial state and covariance matrix
 # Use first 0.04 seconds of raw alpha and GyroY_PU data
-window = 10#int(0.04 / dt) # Size of window for getting sensor readings over
-x_k, P_k = get_new_sensor_readings(0, window)
+window = 4 # Size of window for getting sensor readings over
+start = 0
+x_k, P_k = get_new_sensor_readings(start, start + window)
+R_k = np.array([[1, 0],
+                [0, 1]])
 alpha_KF = [x_k[0]]
 alpha_d_KF = [x_k[1]]
 # Start of loop
-for i in range(window + 1, len(time)):
+for i in range(start + window + 1, len(time)):
     # Step 1: Prediction step
-    # Calculate prediction
     x_k, P_k = get_prediction(F_k, x_k, B_k, u_k, P_k, Q_k)
+
+    # Step 2: Update step
+    # Get new measurement data
+    z_k, R_k = get_new_sensor_readings(i - window, i)
+    # Compute Kalman gain
+    # K = get_kalman_gain(P_k, H_k, R_k)
+    K = np.array([[0.05, 0],
+                  [0, 0.5]])
+    # Update state and uncertainty
+    x_k = get_state_update(x_k, K, z_k, H_k, P_k)
+    P_k = get_covariance_update(I, K, H_k, P_k, R_k)
+    
+    # Store updated state
     alpha_KF.append(x_k[0])
     alpha_d_KF.append(x_k[1])
-    
-    # Step 2: Update step
-    # Get new sensor readings
-    z_k, R_k = get_new_sensor_readings(i - window, i)
-    # Calculate Kalman gain
-    K = get_kalman_gain(P_k, H_k, R_k)
-    # Calculate update
-    x_k, P_k = get_update(x_k, K, z_k, H_k, P_k)
-    # Store updated state
-    # alpha_KF.append(x_k[0])
-    # alpha_d_KF.append(x_k[1])
 
 alpha_KF = np.array(alpha_KF)
 alpha_d_KF = np.array(alpha_d_KF)
-# Plot
-plt.plot(time, alpha)
-plt.plot(time, alpha_CF*15)
-plt.plot(time, AngY_PU)
-plt.plot(time[window:], alpha_KF)
-plt.show()
 
-# plt.plot(time, alpha)
-# plt.plot(time, GyroY_PU)
-# plt.plot(time[window:], alpha_d_KF)
-# plt.show()
+# Plot
+fig, ax = plt.subplots(2)
+ax[0].plot(time[start:], alpha[start:])
+ax[0].plot(time[start+window:], alpha_KF)
+ax[0].plot(time[start+window:], vectorized_integration(alpha_d_KF, dt))
+ax[1].plot(time[start:], GyroY_PU[start:])
+ax[1].plot(time[start+window:], alpha_d_KF)
+ax[0].grid()
+ax[1].grid()
+plt.show()
