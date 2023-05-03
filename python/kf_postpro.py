a6156aa498afca730dc4ebebb7690bee969e2683
c-bruce <callum.bruce1@gmail.com>
2023-05-03 21:04:14 +0100

    Updated variable names and general tidy up

--- python/kf_postpro.py
+++ python/kf_postpro.py
@@ -5,12 +5,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # GET DATA
-df = pd.read_csv('KF_004_mpu6050.csv')
+df = pd.read_csv('KF_001_mpu6050.csv')
 
 # CONSTANTS
 dt = 0.004 # [s]
 freq = 1/dt # [Hz]
-g = 9.81 # [m/s**2]
 
 def get_time(df, dt, freq):
     return np.arange(0, len(df)/freq, dt)
@@ -22,14 +21,14 @@ def vectorized_integration(input_vector, dt):
     return np.array(output_vector[1:])
 
 time = get_time(df, dt, freq)
-z_k_0 = df['z_k_0'].to_numpy()
-z_k_1 = df['z_k_1'].to_numpy()
-z_k_2 = df['z_k_2'].to_numpy()
-z_k_3 = df['z_k_3'].to_numpy()
-x_k_0 = df['x_k_0'].to_numpy()
-x_k_1 = df['x_k_1'].to_numpy()
-x_k_2 = df['x_k_2'].to_numpy()
-x_k_3 = df['x_k_3'].to_numpy()
+z_0 = df['z_0'].to_numpy()
+z_1 = df['z_1'].to_numpy()
+z_2 = df['z_2'].to_numpy()
+z_3 = df['z_3'].to_numpy()
+x_0 = df['x_0'].to_numpy()
+x_1 = df['x_1'].to_numpy()
+x_2 = df['x_2'].to_numpy()
+x_3 = df['x_3'].to_numpy()
 y_0 = df['y_0'].to_numpy()
 y_1 = df['y_1'].to_numpy()
 y_2 = df['y_2'].to_numpy()
@@ -38,35 +37,35 @@ epsilon = df['epsilon'].to_numpy()
 
 # Plot
 fig1, ax1 = plt.subplots(3)
-ax1[0].plot(time, z_k_0)
-ax1[0].plot(time, x_k_0)
+ax1[0].plot(time, z_0)
+ax1[0].plot(time, x_0)
 ax1[0].plot(time, y_0)
-# ax1[0].plot(time, vectorized_integration(x_k_2, dt))
-ax1[1].plot(time, z_k_2)
-ax1[1].plot(time, x_k_2)
+# ax1[0].plot(time, vectorized_integration(x_2, dt))
+ax1[1].plot(time, z_2)
+ax1[1].plot(time, x_2)
 ax1[1].plot(time, y_2)
 ax1[2].plot(time, epsilon)
 ax1[0].grid()
 ax1[1].grid()
 ax1[2].grid()
-ax1[0].set_ylabel("Pitch [deg]")
-ax1[1].set_ylabel("Pitch rate [deg/s]")
+ax1[0].set_ylabel("Roll [deg]")
+ax1[1].set_ylabel("Roll rate [deg/s]")
 ax1[2].set_ylabel("epsilon")
 
 fig2, ax2 = plt.subplots(3)
-ax2[0].plot(time, z_k_1)
-ax2[0].plot(time, x_k_1)
+ax2[0].plot(time, z_1)
+ax2[0].plot(time, x_1)
 ax2[0].plot(time, y_1)
-# ax2[0].plot(time, vectorized_integration(x_k_3, dt))
-ax2[1].plot(time, z_k_3)
-ax2[1].plot(time, x_k_3)
+# ax2[0].plot(time, vectorized_integration(x_3, dt))
+ax2[1].plot(time, z_3)
+ax2[1].plot(time, x_3)
 ax2[1].plot(time, y_3)
 ax2[2].plot(time, epsilon)
 ax2[0].grid()
 ax2[1].grid()
 ax2[2].grid()
-ax2[0].set_ylabel("Roll [deg]")
-ax2[1].set_ylabel("Roll rate [deg/s]")
+ax2[0].set_ylabel("Pitch [deg]")
+ax2[1].set_ylabel("Pitch rate [deg/s]")
 ax2[2].set_ylabel("epsilon")
 
 plt.show()
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
 #include <Wire.h>
-#include <Servo.h>
 #include <BasicLinearAlgebra.h>
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -11,30 +8,23 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Global Variables
-bool imu_started = false;
-bool gyro_calibrated = false;
 long loop_timer;
 
 // Define MPU Variables
 const float acc_scale_factor = 4096; // From MPU6050 datasheet [g]
 const float gyro_scale_factor = 65.5; // From MPU6050 datasheet [deg/s]
-// const float pitch_offset = 2.83; // [deg]
-const float pitch_offset = 0; // [deg]
+const float pitch_offset = 0.0; // [deg]
 const float roll_offset = 0.0; // [deg]
-const float pitch_rate_offset = 0.57; // [deg/s]
+const float pitch_rate_offset = 0.0; // [deg/s]
 const float roll_rate_offset = 0.0; // [deg/s]
 const float rad2deg = 57.2958;
 const int mpu_address = 0x68;
-int gyro_cal_int; // Gyroscope calibration int counter
 float gyro_x_offset, gyro_y_offset, gyro_z_offset; // Gyroscope roll, pitch, yaw calibration offsets
 float AccX, AccY, AccZ, temperature, GyroX, GyroY, GyroZ; // Raw MPU data
 float total_vector_acc;
-float roll_angle_acc, pitch_angle_acc;
-float roll_angle, pitch_angle, yaw_angle;
 
 // Kalman Filter Variables
-// Constant ints and floats
-const int window = 10;
+// Constant floats
 const float dt = 0.004;
 const float fade = 1.0;
 
@@ -45,22 +35,17 @@ float epsilon_max = 5.0;
 float Q_scale_factor = 1000.0;
 
 // Varying matricies
-// BLA::Matrix<4, 1, BLA::Array<4,1,double>> x_k = {0.0, 0.0, 0.0, 0.0}; // State [pitch, roll, pitch_rate, roll_rate]
-BLA::Matrix<4, 1> x_k = {0.0, 0.0, 0.0, 0.0}; // State [pitch, roll, pitch_rate, roll_rate]
-BLA::Matrix<4, 4> P_k = {1.0, 0.0, 0.0, 0.0, 
-                         0.0, 1.0, 0.0, 0.0,
-                         0.0, 0.0, 1.0, 0.0,
-                         0.0, 0.0, 0.0, 1.0}; // Covariance matrix
-BLA::Matrix<4, 1> z_k = {0.0, 0.0, 0.0, 0.0}; // Measurement state [pitch, roll, pitch_rate, roll_rate]
-BLA::Matrix<4, 4> R_k = {5.0, 0.0, 0.0, 0.0, 
-                         0.0, 5.0, 0.0, 0.0,
-                         0.0, 0.0, 5.0, 0.0,
-                         0.0, 0.0, 0.0, 5.0}; // Measurement covariance matrix
-BLA::Matrix<4, 1> y = {0.0, 0.0, 0.0, 0.0}; // Residule [pitch, roll, pitch_rate, roll_rate]
-// BLA::Matrix<4, 4, BLA::Array<4,4,double>> K = {0.0, 0.0, 0.0, 0.0, 
-//                                                0.0, 0.0, 0.0, 0.0,
-//                                                0.0, 0.0, 0.0, 0.0,
-//                                                0.0, 0.0, 0.0, 0.0}; // Kalman gain matrix
+BLA::Matrix<4, 1> x = {0.0, 0.0, 0.0, 0.0}; // State [roll, pitch, roll_rate, pitch_rate]
+BLA::Matrix<4, 1> z = {0.0, 0.0, 0.0, 0.0}; // Measurement state [roll, pitch, roll_rate, pitch_rate]
+BLA::Matrix<4, 1> y = {0.0, 0.0, 0.0, 0.0}; // Residule [roll, pitch, roll_rate, pitch_rate]
+BLA::Matrix<4, 4> P = {1.0, 0.0, 0.0, 0.0, 
+                       0.0, 1.0, 0.0, 0.0,
+                       0.0, 0.0, 1.0, 0.0,
+                       0.0, 0.0, 0.0, 1.0}; // Covariance matrix
+BLA::Matrix<4, 4> Q = {pow(dt, 4) / 4, 0.0, 0.0, 0.0, 
+                       0.0, pow(dt, 4) / 4, 0.0, 0.0,
+                       0.0, 0.0, pow(dt, 2), 0.0,
+                       0.0, 0.0, 0.0, pow(dt, 2)};  // Untracked noise matrix
 BLA::Matrix<4, 4> K = {0.0, 0.0, 0.0, 0.0, 
                        0.0, 0.0, 0.0, 0.0,
                        0.0, 0.0, 0.0, 0.0,
@@ -71,38 +56,27 @@ BLA::Matrix<4, 4> inv = {0.0, 0.0, 0.0, 0.0,
                          0.0, 0.0, 0.0, 0.0}; // Inverse matrix required to calculate K
 
 // Constant matricies
-const BLA::Matrix<4, 4> F_k = {1.0, 0.0, dt, 0.0, 
-                               0.0, 1.0, 0.0, dt,
-                               0.0, 0.0, 1.0, 0.0,
-                               0.0, 0.0, 0.0, 1.0}; // State transition matrix
-const BLA::Matrix<4, 4> B_k = {0.0, 0.0, 0.0, 0.0, 
-                               0.0, 0.0, 0.0, 0.0,
-                               0.0, 0.0, 0.0, 0.0,
-                               0.0, 0.0, 0.0, 0.0}; // Control matrix
-const BLA::Matrix<4, 1> u_k = {0.0, 0.0, 0.0, 0.0}; // Control vector
-BLA::Matrix<4, 4> Q_k = {pow(dt, 4) / 4, 0.0, 0.0, 0.0, 
-                         0.0, pow(dt, 4) / 4, 0.0, 0.0,
-                         0.0, 0.0, pow(dt, 2), 0.0,
-                         0.0, 0.0, 0.0, pow(dt, 2)};  // Untracked noise matrix
-// const BLA::Matrix<4, 4> Q_k = {3.2e-10, 0.0, 1.6e-7, 0.0, 
-//                                0.0, 3.2e-10, 0.0, 1.6e-7,
-//                                1.6e-7, 0.0, 8.0e-5, 0.0,
-//                                0.0, 1.6e-7, 0.0, 8.0e-5};  // Untracked noise matrix
-const BLA::Matrix<4, 4> H_k = {1.0, 0.0, 0.0, 0.0, 
-                               0.0, 1.0, 0.0, 0.0,
-                               0.0, 0.0, 1.0, 0.0,
-                               0.0, 0.0, 0.0, 1.0}; // Transformation matrix
+const BLA::Matrix<4, 1> u = {0.0, 0.0, 0.0, 0.0}; // Control vector
+const BLA::Matrix<4, 4> R = {10.0, 0.0, 0.0, 0.0, 
+                             0.0, 10.0, 0.0, 0.0,
+                             0.0, 0.0, 10.0, 0.0,
+                             0.0, 0.0, 0.0, 10.0}; // Measurement covariance matrix
+const BLA::Matrix<4, 4> F = {1.0, 0.0, dt, 0.0, 
+                             0.0, 1.0, 0.0, dt,
+                             0.0, 0.0, 1.0, 0.0,
+                             0.0, 0.0, 0.0, 1.0}; // State transition matrix
+const BLA::Matrix<4, 4> B = {0.0, 0.0, 0.0, 0.0, 
+                             0.0, 0.0, 0.0, 0.0,
+                             0.0, 0.0, 0.0, 0.0,
+                             0.0, 0.0, 0.0, 0.0}; // Control matrix
+const BLA::Matrix<4, 4> H = {1.0, 0.0, 0.0, 0.0, 
+                             0.0, 1.0, 0.0, 0.0,
+                             0.0, 0.0, 1.0, 0.0,
+                             0.0, 0.0, 0.0, 1.0}; // Observation matrix
 const BLA::Matrix<4, 4> I = {1.0, 0.0, 0.0, 0.0, 
                              0.0, 1.0, 0.0, 0.0,
                              0.0, 0.0, 1.0, 0.0,
                              0.0, 0.0, 0.0, 1.0}; // Identity matrix
-
-// Arrays to store state data
-float z_pitch_array[window];
-float z_roll_array[window];
-float z_pitch_rate_array[window];
-float z_roll_rate_array[window];
-byte index = 0;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // MPU
@@ -143,144 +117,80 @@ void readMPUdata()
   GyroX = -(Wire.read() << 8 | Wire.read());
   GyroY = -(Wire.read() << 8 | Wire.read());
   GyroZ = (Wire.read() << 8 | Wire.read());
+  // AccX = Wire.read() << 8 | Wire.read();
+  // AccY = Wire.read() << 8 | Wire.read();
+  // AccZ = Wire.read() << 8 | Wire.read();
+  // temperature = Wire.read() << 8 | Wire.read();
+  // GyroX = Wire.read() << 8 | Wire.read();
+  // GyroY = Wire.read() << 8 | Wire.read();
+  // GyroZ = Wire.read() << 8 | Wire.read();
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Kalman Filter
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-float get_covariance(const float * a, const float * b, const int n)
-{
-  // Calculate averages
-  float average_a = 0.0;
-  float average_b = 0.0;
-  for (int i = 0; i < n; i++)
-  {
-    average_a += a[i];
-    average_b += b[i];
-  }
-  average_a /= n;
-  average_b /= n;
-
-  // Calculate covariance
-  float covariance = 0.0;
-  for (int i = 0; i < n; i++)
-  {
-    covariance += (a[i] - average_a) * (b[i] - average_b);
-  }
-  covariance /= n;
-
-  return covariance;
-}
-
-void get_z_k()
+void get_z()
 {
   total_vector_acc = sqrt((AccX * AccX) + (AccY * AccY) + (AccZ * AccZ)); // Calculate the total accelerometer vector
 
   if(abs(AccY) < total_vector_acc) // Prevent asin function producing a NaN
   {
-    z_k.storage(0, 1) = asin((float)AccY/total_vector_acc) * -rad2deg; // roll
+    z.storage(0, 0) = asin((float)AccY/total_vector_acc) * -rad2deg; // roll
   }
   if(abs(AccX) < total_vector_acc) // Prevent asin function producing a NaN
   {
-    z_k.storage(0, 0) = asin((float)AccX/total_vector_acc) * rad2deg; // pitch
+    z.storage(0, 1) = asin((float)AccX/total_vector_acc) * rad2deg; // pitch
   }
-  // z_k.storage(0, 0) = ((atan2(AccZ, AccX) * rad2deg) + 90) - pitch_offset;
-  // z_k.storage(0, 1) = -(((atan2(AccZ, AccY) * rad2deg)+ 90 ) - roll_offset);
-  // z_k.storage(0, 0) = ((atan2(AccZ, AccX) * rad2deg) + 90) - pitch_offset;
-  // z_k.storage(0, 1) = ((atan2(AccZ, AccY) * rad2deg) + 90 ) - roll_offset;
-  z_k.storage(0, 2) = (GyroY / gyro_scale_factor) - pitch_rate_offset;
-  z_k.storage(0, 3) = (GyroX / gyro_scale_factor) - roll_rate_offset;
-}
-
-void get_R_k()
-{
-  R_k.storage(0, 0) = get_covariance(z_pitch_array, z_pitch_array, window);
-  R_k.storage(0, 1) = get_covariance(z_pitch_array, z_roll_array, window);
-  R_k.storage(0, 2) = get_covariance(z_pitch_array, z_pitch_rate_array, window);
-  R_k.storage(0, 3) = get_covariance(z_pitch_array, z_roll_rate_array, window);
-  //
-  R_k.storage(1, 0) = get_covariance(z_roll_array, z_pitch_array, window);
-  R_k.storage(1, 1) = get_covariance(z_roll_array, z_roll_array, window);
-  R_k.storage(1, 2) = get_covariance(z_roll_array, z_pitch_rate_array, window);
-  R_k.storage(1, 3) = get_covariance(z_roll_array, z_roll_rate_array, window);
-  //
-  R_k.storage(2, 0) = get_covariance(z_pitch_rate_array, z_pitch_array, window);
-  R_k.storage(2, 1) = get_covariance(z_pitch_rate_array, z_roll_array, window);
-  R_k.storage(2, 2) = get_covariance(z_pitch_rate_array, z_pitch_rate_array, window);
-  R_k.storage(2, 3) = get_covariance(z_pitch_rate_array, z_roll_rate_array, window);
-  //
-  R_k.storage(3, 0) = get_covariance(z_roll_rate_array, z_pitch_array, window);
-  R_k.storage(3, 1) = get_covariance(z_roll_rate_array, z_roll_array, window);
-  R_k.storage(3, 2) = get_covariance(z_roll_rate_array, z_pitch_rate_array, window);
-  R_k.storage(3, 3) = get_covariance(z_roll_rate_array, z_roll_rate_array, window);
+  z.storage(0, 2) = (GyroX / gyro_scale_factor) - roll_rate_offset; // roll rate
+  z.storage(0, 3) = (GyroY / gyro_scale_factor) - pitch_rate_offset; // pitch rate
 }
 
 void get_new_sensor_readings()
 {
-  // Step 1: Get raw mpu data
   readMPUdata();
 
-  // Step 2: Calculate new z_k
-  get_z_k();
-
-  // Step 3: Push new z_k into storage arrays at index
-  z_pitch_array[index] = z_k.storage(0, 0);
-  z_roll_array[index] = z_k.storage(0, 1);
-  z_pitch_rate_array[index] = z_k.storage(0, 2);
-  z_roll_rate_array[index] = z_k.storage(0, 3);
-
-  // Step 4: Iterate index
-  if (index < window) {
-    index++;
-  }
-  else if (index == window) {
-    index = 0;
-  }
-  
-  // Step 5: Calculate R_k
-  // get_R_k();
+  get_z();
 }
 
 void get_prediction()
 {
-  x_k = (F_k * x_k) + (B_k * u_k);
-  P_k = ((F_k * (P_k * (~F_k))) * pow((pow(fade, 2)), 0.5)) + Q_k;
-  // P_k = ((F_k * (P_k * (~F_k))) * 1.0) + Q_k;
+  x = (F * x) + (B * u);
+  P = ((F * (P * (~F))) * pow((pow(fade, 2)), 0.5)) + Q;
 }
 
 void get_kalman_gain()
 {
-  inv = BLA::Inverse((H_k * (P_k * (~H_k))) + R_k);
-  K = ((P_k * (~H_k)) * inv);
+  inv = BLA::Inverse((H * (P * (~H))) + R);
+  K = ((P * (~H)) * inv);
 }
 
 void get_update()
 {
-  x_k = x_k + (K * (z_k - (H_k * x_k)));
-  P_k = (((I - (K * H_k)) * P_k) * (~(I - (K * H_k)))) + ((K * R_k) * (~K));
+  x = x + (K * (z - (H * x)));
+  P = (((I - (K * H)) * P) * (~(I - (K * H)))) + ((K * R) * (~K));
 }
 
 void get_residual()
 {
-  y = z_k - x_k;
+  y = z - x;
 }
 
 void get_epsilon()
 {
-  epsilon = (~y * (inv * y)); // Need to fix this
+  epsilon = (~y * (inv * y));
 }
 
 void scale_Q()
 {
   if (epsilon.storage(0, 0) > epsilon_max)
   {
-    Q_k *= Q_scale_factor;
+    Q *= Q_scale_factor;
     count += 1;
   }
   else if (count > 0)
   {
-    Q_k /= Q_scale_factor;
+    Q /= Q_scale_factor;
     count -= 1;
   }
 }
@@ -299,21 +209,21 @@ void setup()
   // Serial.print("theta");
   // Serial.print(",");
   // Serial.println("psi");
-  Serial.print("z_k_0");
+  Serial.print("z_0");
   Serial.print(",");
-  Serial.print("z_k_1");
+  Serial.print("z_1");
   Serial.print(",");
-  Serial.print("z_k_2");
+  Serial.print("z_2");
   Serial.print(",");
-  Serial.print("z_k_3");
+  Serial.print("z_3");
   Serial.print(",");
-  Serial.print("x_k_0");
+  Serial.print("x_0");
   Serial.print(",");
-  Serial.print("x_k_1");
+  Serial.print("x_1");
   Serial.print(",");
-  Serial.print("x_k_2");
+  Serial.print("x_2");
   Serial.print(",");
-  Serial.print("x_k_3");
+  Serial.print("x_3");
   Serial.print(",");
   Serial.print("y_0");
   Serial.print(",");
@@ -344,26 +254,27 @@ void loop()
   // Continuous adjustment step
   get_epsilon();
   scale_Q();
-  // Serial.print(x_k.storage(0, 1)); // phi
+
+  // Serial.print(x.storage(0, 1)); // phi
   // Serial.print(",");
-  // Serial.print(x_k.storage(0, 0)); // theta
+  // Serial.print(x.storage(0, 0)); // theta
   // Serial.print(",");
   // Serial.println("0"); // psi
-  Serial.print(z_k.storage(0, 0));
+  Serial.print(z.storage(0, 0));
   Serial.print(",");
-  Serial.print(z_k.storage(0, 1));
+  Serial.print(z.storage(0, 1));
   Serial.print(",");
-  Serial.print(z_k.storage(0, 2));
+  Serial.print(z.storage(0, 2));
   Serial.print(",");
-  Serial.print(z_k.storage(0, 3));
+  Serial.print(z.storage(0, 3));
   Serial.print(",");
-  Serial.print(x_k.storage(0, 0));
+  Serial.print(x.storage(0, 0));
   Serial.print(",");
-  Serial.print(x_k.storage(0, 1));
+  Serial.print(x.storage(0, 1));
   Serial.print(",");
-  Serial.print(x_k.storage(0, 2));
+  Serial.print(x.storage(0, 2));
   Serial.print(",");
-  Serial.print(x_k.storage(0, 3));
+  Serial.print(x.storage(0, 3));
   Serial.print(",");
   Serial.print(y.storage(0, 0));
   Serial.print(",");
@@ -375,6 +286,5 @@ void loop()
   Serial.print(",");
   Serial.println(epsilon.storage(0, 0));
   
-  // Serial.println(micros() - loop_timer);
   while(micros() - loop_timer < 4000); // Wait until the loop_timer reaches 4000us (250Hz) before starting the next loop
 }
